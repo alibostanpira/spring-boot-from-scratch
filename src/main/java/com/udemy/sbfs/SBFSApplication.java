3c35c42815264a70744231fc14d0d1e42bed658f
alibostanpira <alibostanpira@gmail.com>
2022-05-31 17:57:56 +0200

    Git configuration is done.

---
+++ src/main/java/com/udemy/sbfs/SBFSApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.sbfs;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBFSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SBFSApplication.class, args);
+    }
+}
