1518ca882306cb0946cc94c0170378591b797230
alibostanpira <alibostanpira@gmail.com>
2022-06-01 02:13:10 +0200

    Easy School Home Page is done.

---
+++ src/test/java/com/udemy/sbfs/SBFSApplicationTests.java
@@ -0,0 +1,14 @@
+package com.udemy.sbfs;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SBFSApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
